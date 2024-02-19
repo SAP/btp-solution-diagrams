39a5388e7df8fe93559474b8f93717288ac1cec9
Fabian Lehmann <fabian.lehmann02@sap.com>
2024-02-19 07:16:03 +0100

    update baseurl for page

--- guideline/docusaurus.config.ts
+++ guideline/docusaurus.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   //baseUrl: '/sap/btp-diagram-guideline',
-  baseUrl: '/btp-diagram-guideline/',
+  baseUrl: '/',
   githubHost:'github.com',
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
