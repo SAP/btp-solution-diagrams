f3671f1406bc6a6f1fba3d12b945fc9caeba013c
Christian Lechner <lechnerc77@users.noreply.github.com>
2025-05-21 09:30:48 +0200

    chore: cleanup of tsx files (#221)
    Co-authored-by: Christian Lechner <22294087+lechnerc77@users.noreply.github.com>

--- guideline/src/sections/Icons.tsx
+++ guideline/src/sections/Icons.tsx
@@ -7,44 +7,43 @@ import '@ui5/webcomponents-icons/dist/AllIcons';
 export default function OverviewSection(): JSX.Element {
   return (
     <section >
-         <div className="container">
+      <div className="container">
 
-        
-         <div className="icons_body">
-         <h2 className="icons_header">
-                        <b >
-                        <HighlightText>Icons</HighlightText>
-                       </b>     
-                    </h2>
-        <div className="icons">
-       
-            
+
+        <div className="icons_body">
+          <h2 className="icons_header">
+            <b >
+              <HighlightText>Icons</HighlightText>
+            </b>
+          </h2>
+          <div className="icons">
+
+
             <div className='icons_image_btp'>
-            <Link className="button_sapicons" to="https://github.com/SAP/btp-solution-diagrams/tree/main/assets/shape-libraries-and-editable-presets/draw.io/20-02-99-sap-btp-service-icons-all">
-            <Button className="button_sapicons_ui5" design="Emphasized" style={{ width: 150 }}>Get it !</Button>
-            </Link>
+              <Link className="button_sapicons" to="https://github.com/SAP/btp-solution-diagrams/tree/main/assets/shape-libraries-and-editable-presets/draw.io/20-02-99-sap-btp-service-icons-all">
+                <Button className="button_sapicons_ui5" design="Emphasized" style={{ width: 150 }}>Get it !</Button>
+              </Link>
             </div>
-            
-            
+
+
             <div className='icons_image_generic'>
-            <Link className="button_generic" to="https://github.com/SAP/btp-solution-diagrams/tree/main/assets/shape-libraries-and-editable-presets/draw.io/20-03-generic-icons">
-            <Button className="button_generic_ui5" design="Emphasized" style={{ width: 150 }}>Get it !</Button>
-            </Link>
+              <Link className="button_generic" to="https://github.com/SAP/btp-solution-diagrams/tree/main/assets/shape-libraries-and-editable-presets/draw.io/20-03-generic-icons">
+                <Button className="button_generic_ui5" design="Emphasized" style={{ width: 150 }}>Get it !</Button>
+              </Link>
             </div>
-           
-            </div>
-            </div>
-            </div>
-            
-            
+
+          </div>
+        </div>
+      </div>
+
+
     </section>
   )
 }
 export function HighlightText(props) {
-    return (
-      <strong className="bolder relative z-10 box-content before:absolute before:bottom-0 before:z-[-1] before:h-3 before:w-full before:bg-[#95DAFF50] before:duration-300 before:content-[''] hover:before:w-0">
-        {props.children}
-      </strong>
-    )
-  }
-
+  return (
+    <strong className="bolder relative z-10 box-content before:absolute before:bottom-0 before:z-[-1] before:h-3 before:w-full before:bg-[#95DAFF50] before:duration-300 before:content-[''] hover:before:w-0">
+      {props.children}
+    </strong>
+  )
+}
--- guideline/src/sections/Whats.tsx
+++ guideline/src/sections/Whats.tsx
@@ -1,38 +1,33 @@
 
 import React from 'react'
-import clsx from 'clsx';
-import useBaseUrl from '@docusaurus/useBaseUrl'
-import ThemedImage from '@theme/ThemedImage'
 import Link from '@docusaurus/Link';
-import { Button } from '@ui5/webcomponents-react';
 import '@ui5/webcomponents-icons/dist/AllIcons';
 
 
 export default function OverviewSection(): JSX.Element {
   return (
     <section >
-        <br />
-                
-                <div className="container">
-                <div className='whats_banner'>        
-                
-                    <div className="whats_body">
-                    <p className="whats_body_p">+++ Recent Updates +++  Draw.io libraries have been updated. Check out where to find our new content <Link to="https://github.com/SAP/btp-solution-diagrams/releases" target="_blank">here </Link>!<br /></p>
-                    </div> 
-                    
-                    </div>
-                    
-                    </div>
-                
-        
+      <br />
+
+      <div className="container">
+        <div className='whats_banner'>
+
+          <div className="whats_body">
+            <p className="whats_body_p">+++ Recent Updates +++  Draw.io libraries have been updated. Check out where to find our new content <Link to="https://github.com/SAP/btp-solution-diagrams/releases" target="_blank">here </Link>!<br /></p>
+          </div>
+
+        </div>
+
+      </div>
+
+
     </section>
   )
 }
 export function HighlightText(props) {
-    return (
-      <strong className="bolder relative z-10 box-content before:absolute before:bottom-0 before:z-[-1] before:h-3 before:w-full before:bg-[#95DAFF50] before:duration-300 before:content-[''] hover:before:w-0">
-        {props.children}
-      </strong>
-    )
-  }
-
+  return (
+    <strong className="bolder relative z-10 box-content before:absolute before:bottom-0 before:z-[-1] before:h-3 before:w-full before:bg-[#95DAFF50] before:duration-300 before:content-[''] hover:before:w-0">
+      {props.children}
+    </strong>
+  )
+}
