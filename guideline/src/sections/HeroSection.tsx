28562fcaf849ab7b7da3ccf15bd6e5f6cb956e25
Christian Lechner <22294087+lechnerc77@users.noreply.github.com.>
2025-05-21 09:28:35 +0200

    chore: cleanup of tsx files

--- guideline/src/sections/HeroSection.tsx
+++ guideline/src/sections/HeroSection.tsx
@@ -1,8 +1,5 @@
 
 import React from 'react'
-import clsx from 'clsx';
-import useBaseUrl from '@docusaurus/useBaseUrl'
-import ThemedImage from '@theme/ThemedImage'
 import Link from '@docusaurus/Link';
 import { Button } from '@ui5/webcomponents-react';
 import '@ui5/webcomponents-icons/dist/AllIcons';
@@ -12,30 +9,30 @@ export default function HeroSection(): JSX.Element {
   return (
     <section >
        <br />
-        <div className='hero_banner'>        
+        <div className='hero_banner'>
         <div className="container">
-        
+
         <div className="welcome">
             <h1>
                 <b className="header_text" >
                    BTP Solution Diagram <br /> Repository
-               </b>     
+               </b>
             </h1>
             <div className="header_body">
                 <p className="header_body_p">Welcome to the single source of truth for BTP Architects.<br />
                 The repository includes latest updates and ready-to-use templates<br />to develop high quality diagrams of architectural landscapes.
                  </p>
-            </div> 
+            </div>
             <Link to="https://github.com/SAP/btp-solution-diagrams/tree/main/assets/all-in-one-starter-kits" target="_blank">
             <Button style={{ width: 150 }}>Get Starter Kit</Button>
             </Link>
-            
-           
+
+
             </div>
-            
-        </div> 
+
         </div>
-        
+        </div>
+
     </section>
   )
 }
@@ -48,6 +45,3 @@ export function HighlightText(props) {
       </strong>
     )
   }
-
-
-
--- guideline/src/sections/Libraries.tsx
+++ guideline/src/sections/Libraries.tsx
@@ -7,52 +7,51 @@ import '@ui5/webcomponents-icons/dist/AllIcons';
 export default function OverviewSection(): JSX.Element {
   return (
     <section >
-        <div className="container">
+      <div className="container">
         <div className="libraries">
-            
-            
-            <div className="libraries_body">
+
+
+          <div className="libraries_body">
             <h2>
-                <b >
-                <HighlightText>What software do I need to work with  <br/> BTP Solution Diagrams? </HighlightText>
-               </b>     
+              <b >
+                <HighlightText>What software do I need to work with  <br /> BTP Solution Diagrams? </HighlightText>
+              </b>
             </h2>
-            <p>Currently, editable BTP Solution Diagram templates <br/> 
-            are only provided for draw.io and PowerPoint. <br/> 
-            We hope to extend the support to other editors in the future,<br/> 
-            so please send us any feedback and ideas to help us in this direction.<br/> 
+            <p>Currently, editable BTP Solution Diagram templates <br />
+              are only provided for draw.io and PowerPoint. <br />
+              We hope to extend the support to other editors in the future,<br />
+              so please send us any feedback and ideas to help us in this direction.<br />
             </p>
 
             <Link className="button_drawio" to="https://sap.github.io/btp-solution-diagrams/docs/solution_diagr_intro/intro/">
-            <Button className="button_drawio2" design="Emphasized" style={{ width: 150 }}>draw.io</Button>
+              <Button className="button_drawio2" design="Emphasized" style={{ width: 150 }}>draw.io</Button>
             </Link>
 
             <Link className="button_lucid" to="https://lucid.app/marketplace#/listing/8e327624-ad9a-4ccf-b74c-325bb907a0ef">
-            <Button className="button_lucid2" design="Emphasized" style={{ width: 150 }}>Lucidchart</Button>
+              <Button className="button_lucid2" design="Emphasized" style={{ width: 150 }}>Lucidchart</Button>
             </Link>
 
             <Link className="button_ppt" to="https://dam.sap.com/mac/u/a/e7KMGSu.htm?rc=10&includeRelatedAssets=true">
-            <Button design="Emphasized" style={{ width: 150 }}>Powerpoint</Button>
+              <Button design="Emphasized" style={{ width: 150 }}>Powerpoint</Button>
             </Link>
-     
-                    </div> 
-           
-            <div className='libraries_image'>
-                <img src="img/drawIO.svg" />
-            </div>
-            </div>
-            <div className="footer_div">
-            </div>
 
-            </div>
+          </div>
+
+          <div className='libraries_image'>
+            <img src="img/drawIO.svg" />
+          </div>
+        </div>
+        <div className="footer_div">
+        </div>
+
+      </div>
     </section>
   )
 }
 export function HighlightText(props) {
-    return (
-      <strong className="bolder relative z-10 box-content before:absolute before:bottom-0 before:z-[-1] before:h-3 before:w-full before:bg-[#95DAFF50] before:duration-300 before:content-[''] hover:before:w-0">
-        {props.children}
-      </strong>
-    )
-  }
-
+  return (
+    <strong className="bolder relative z-10 box-content before:absolute before:bottom-0 before:z-[-1] before:h-3 before:w-full before:bg-[#95DAFF50] before:duration-300 before:content-[''] hover:before:w-0">
+      {props.children}
+    </strong>
+  )
+}
